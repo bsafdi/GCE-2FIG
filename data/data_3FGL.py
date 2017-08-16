04dede21ff19327077c533b8c775be5ff24de2d6
Siddharth Mishra Sharma <smsharma@feynman.princeton.edu>
2017-08-16 14:30:19 -0400

    Added scan interface

---
+++ data/data_3FGL.py
@@ -0,0 +1,53 @@
+###############################################################################
+# data_3FGL.py
+###############################################################################
+#
+# Bin 3FGL psr candidates using file from Mattia
+#
+# Written: Nick Rodd, MIT, 7 August 2017
+#
+###############################################################################
+
+import numpy as np
+from astropy.io import fits
+
+# Load Mattia catalog
+load = fits.open('./2FIG_Pass8_Arxiv.fits')
+cat = np.array(load[1].data)
+
+glon = np.array([])
+glat = np.array([])
+eflux = np.array([])
+
+count = 0
+
+for i in range(len(cat)):
+    # Check if PSR or psr
+    if (cat[i][22] == 'PSR') | (cat[i][22] == 'psr'):
+        glon = np.append(glon,cat[i][3])
+        glat = np.append(glat,cat[i][4])
+        eflux = np.append(eflux,cat[i][11])
+        count += 1
+
+print count
+
+# Now bin
+
+angbins = -np.linspace(-20,20,13) # - because top left is 0,0 and astro longitude
+fluxbins = np.append(np.logspace(-6,-5,7), np.logspace(-5,-4,3)[1:3])
+
+PSR_data = np.zeros((12,12,8))
+
+for ips in range(len(glon)):
+    lval = glon[ips]
+    bval = glat[ips]
+    fval = eflux[ips]
+    for li in range(12):
+        if (lval <= angbins[li]) & (lval > angbins[li+1]):
+            for bi in range(12):
+                if (bval <= angbins[bi]) & (bval > angbins[bi+1]):
+                    for fi in range(8):
+                        if (fval >= fluxbins[fi]) & (fval < fluxbins[fi+1]):
+                            PSR_data[li,bi,fi] += 1.
+
+np.save('./PSR_data_3fgl',PSR_data)
